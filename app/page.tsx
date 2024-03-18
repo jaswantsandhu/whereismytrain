bf0f39e170deeb107a6ef86c52ec08a3123f1f35
Jaswant Sandhu <jaswant.sandhu88@gmail.com>
2024-03-18 12:47:13 +0000

    Station update.

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Footer from "./components/footer";
 
 export default function Home() {
   const [termsAccepted, setTermsAccepted] = useState<null | boolean>(null);
-  const [selectedCRS, setSelectedCRS] = useState<string>("LST");
+  const [selectedCRS, setSelectedCRS] = useState<string>("");
 
   return (
     <main className="flex min-h-screen flex-col p-10">
@@ -17,8 +17,9 @@ export default function Home() {
             className="custom-select bg-black text-white border-2 border-yellow-500 p-2 rounded-none"
             value={selectedCRS}
             onChange={(e) => setSelectedCRS(e.target.value)}
-            aria-label="Select Station"
+            aria-label="Please select station"
           >
+            <option disabled value={""}>Please select station</option>
             <optgroup label="London">
               <option value={"LST"}>London Liverpool Street</option>
               <option value={"KGX"}>London Kings Cross</option>
@@ -26,6 +27,8 @@ export default function Home() {
               <option value={"EUS"}>London Euston</option>
               <option value={"PAD"}>London Paddington</option>
               <option value={"WAT"}>London Waterloo</option>
+              <option value={"SRA"}>London Stratford</option>
+              <option value={"SFA"}>Stratford International</option>
             </optgroup>
             <optgroup label="Kent">
               <option value={"SEV"}>Sevenoaks</option>
@@ -45,8 +48,11 @@ export default function Home() {
             </optgroup>
           </select>
 
-          <DepartureBoard crs={selectedCRS} />
-          <p className="mt-2">Auto refresh: <span className="text-red-600">Off</span> <span className="text-neutral-700">(Upcoming)</span></p>
+          {selectedCRS !== "" && <DepartureBoard crs={selectedCRS} />}
+          <p className="mt-2">
+            Auto refresh: <span className="text-red-600">Off</span>{" "}
+            <span className="text-neutral-700">(Upcoming)</span>
+          </p>
         </>
       )}
       <TermsAndPrivacyButtons onDecision={setTermsAccepted} />
